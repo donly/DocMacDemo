d30bb49f92ee791261d5099bb16a3683d9332bff
Donglin <1032894940@qq.com>
2020-07-02 18:49:39 +0800

    Initial Commit

---
+++ DocMacDemo/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DocMacDemo
+//
+//  Created by MareCrisium on 2/7/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
